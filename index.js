c347bcf236be65ae7ddd91add994b34669f71510
DeeBest <simphiwedladla8@gmail.com>
2024-05-02 07:37:45 +0200

    added change event listeners to the inputs that call the updateTemp function when triggered

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const currentYear = new Date().getFullYear();
 
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
+
+const celsius = document.getElementById('celsius');
+const fahrenheit = document.getElementById('fahrenheit');
+const kelvin = document.getElementById('kelvin');
+
+celsius.addEventListener('change', updateTemp);
+
+kelvin.addEventListener('change', updateTemp);
+
+fahrenheit.addEventListener('change', updateTemp);
+
+function updateTemp(e) {
+  console.log(e.target.id);
+}
