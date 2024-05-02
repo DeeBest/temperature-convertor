fb34f08c114b74cbc0bf0666fcd96dc33e9b59e8
DeeBest <simphiwedladla8@gmail.com>
2024-05-02 09:37:57 +0200

    updated README file

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# temperature-convertor
+# Temperature Convertor
+
+![screenshot1 of the app](./images/Screenshot1.png)
+
+Check it _**LIVE**_ 👉
+**<https://deebest.github.io/temperature-convertor/>**
+
+A _**Temperature Convertor**_ web application I built with HTML,CSS and JavaScript, I built it as a part of my JavaScript learning journey.
+
+![screenshot2 of the app](./images/Screenshot2.png)
+
+## Uses
+
+- You can convert **CELSIUS**, **FAHRENHEIT** & **KELVIN** same time dynamically _!!_
+
+## Tech Stack Used
+
+- GIT
+- HTML
+- CSS
+- JavaScript
+
+## Get In Touch 👇
+
+Email : **<simphiwedladla8@gmail.com>**
+
+Linked: **<https://www.linkedin.com/in/dladla-simphiwe-89061a20a/>**
+
+Github: **<https://github.com/DeeBest>**
+
+Portfolio: **<https://deebest.github.io/personal-webpage/>**
--- index.js
+++ index.js
@@ -8,28 +8,40 @@ const fahrenheit = document.getElementById('fahrenheit');
 const kelvin = document.getElementById('kelvin');
 
 celsius.addEventListener('change', updateTemp);
+celsius.addEventListener('input', updateTemp);
 
 kelvin.addEventListener('change', updateTemp);
+kelvin.addEventListener('input', updateTemp);
 
 fahrenheit.addEventListener('change', updateTemp);
+fahrenheit.addEventListener('input', updateTemp);
 
 function updateTemp(e) {
   const currentTempValue = +e.target.value;
 
   switch (e.target.id) {
     case 'celsius':
-      kelvin.value = (currentTempValue + 273.32).toFixed(2);
+      kelvin.value = (currentTempValue + 273.15).toFixed(2);
       fahrenheit.value = (currentTempValue * 1.8 + 32).toFixed(2);
       break;
     case 'fahrenheit':
       celsius.value = ((currentTempValue - 32) / 1.8).toFixed(2);
-      kelvin.value = ((currentTempValue - 32) / 1.8 + 273.32).toFixed(2);
+      kelvin.value = ((currentTempValue - 32) / 1.8 + 273.15).toFixed(2);
       break;
     case 'kelvin':
-      celsius.value = (currentTempValue - 273.32).toFixed(2);
-      fahrenheit.value = ((currentTempValue - 273.32) * 1.8 + 32).toFixed(2);
+      celsius.value = (currentTempValue - 273.15).toFixed(2);
+      fahrenheit.value = ((currentTempValue - 273.15) * 1.8 + 32).toFixed(2);
       break;
     default:
       break;
   }
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+  const body = document.body;
+  if (body.scrollHeight <= window.innerHeight) {
+    body.style.overflow = 'hidden';
+  } else {
+    body.style.overflow = 'auto';
+  }
+});
